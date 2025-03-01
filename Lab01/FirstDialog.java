af9a0a047822b2fb40453a40359f799c45821362
Nguyễn Trung Kiên <you@example.com>
2025-03-01 09:09:00 +0700

    add FirstDialog.java

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class FirstDialog{
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null,"202235759 Nguyen Trung Kien\n");
+		System.exit(0);
+	}
+}
