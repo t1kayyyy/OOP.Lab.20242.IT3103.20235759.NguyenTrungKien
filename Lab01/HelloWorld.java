241818dac23137d8ea86760f1e6c783613bcd2e8
Nguyễn Trung Kiên <you@example.com>
2025-03-01 08:54:28 +0700

    change source

--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("20235759\n");
 		System.out.println("xin chao cac ban\n");
+		System.out.println("Hello World!\n");
 	}
 }
